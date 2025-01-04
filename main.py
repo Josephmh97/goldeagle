1aa6207b388b07ee292a99ed4a6b59f66701a8d7
Youcef <43858722+Josephmh97@users.noreply.github.com>
2025-01-04 12:44:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ with open('data.txt', 'r') as file:
 
 # Fixed values for the /tap request
 available_taps = 1000
-count = 512
+count = 503
 
 # Loop to send requests for each token, alternating between /tap and /wallet/claim
 while True:
